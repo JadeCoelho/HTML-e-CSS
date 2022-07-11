adb81eda881b27f849510b0771795c38daee683c
Jade <jademfcoelho@gmail.com>
2022-07-11 19:33:45 -0300

    aulas js cfb cursos

---
+++ html (cfbcursos)/js/aula3.js
@@ -0,0 +1,11 @@
+var canal = 'cfb cursos'
+canal = 'js'
+document.write(canal)
+
+const nome = 'guelinson'
+document.write(nome)
+const quebra ='\n'
+
+/*alert("cfb cursos - curso de js completo e profissional")*/
+alert(canal + " " + nome + quebra + 10)
+
